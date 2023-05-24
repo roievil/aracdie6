c414e8f246ba21573babb1713354ad5f7d36dc53
Olivier Glorieux <roievil@yahoo.com>
2023-05-24 23:59:33 +0200

    remove next theme

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,9 @@
 import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import { GiTie } from "react-icons/gi";
 import { GoLocation } from "react-icons/go";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 const Sidebar = () => {
-  const { theme, setTheme } = useTheme();
-
-  const changeTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
   return (
     <>
       <Image
@@ -61,13 +54,6 @@ const Sidebar = () => {
         onClick={() => window.open("mailto:code.sumax@gmail.com")}
       >
         Email me
-      </button>
-      <button
-        onClick={changeTheme}
-        className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 "
-      >
-        {/* //TODO remove bg black */}
-        Toggle Theme
       </button>
     </>
   );
