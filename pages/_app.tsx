b23b6cb897105f7432dc6d966c8c11378c0e0cb9
Olivier Glorieux <roievil@yahoo.com>
2024-01-25 18:12:57 +0100

    prod 5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import GlobalProvider from "../context/GlobalContext";
 
+//zinc-200
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <div className="grid p-5  md:p-12 ">
-        <div className="border  bg-zinc-200 border-gray-400 flex flex-col col-span-12 overflow-hidden shadow-custom-light rounded-2xl lg:col-span-9">
+        <div className="border  bg-white border-gray-400 flex flex-col col-span-12 overflow-hidden shadow-custom-light rounded-2xl lg:col-span-9">
           <GlobalProvider>
             {/* //!navbar */}
             <Navbar />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
 import ServiceCard from "../components/ServiceCard";
 import { services } from "../data";
+import bannner1 from "../public/images/accueil/arcadieBanner1.png";
+import bannner2 from "../public/images/accueil/arcadieBanner2.png";
+import Image from "next/image";
 
 const About: NextPage = () => {
   return (
@@ -8,31 +11,45 @@ const About: NextPage = () => {
       className="flex flex-col flex-grow px-6 bg-cover bg-bottom overflow-y-scroll"
       style={{
         height: "80vh",
-        backgroundImage: "url('./images/accueil/arcadieBanner.png')",
+        backgroundImage: "url('./images/accueil/arcadieBannerFond.png')",
       }}
     >
       <div
-        className="flex-grow p-4 relative"
-        style={{
-          marginLeft: "-1.5rem",
-          marginRight: "-1.5rem",
-        }}
+        className="flex-grow p-4 relative flex flex-col justify-center items-center"
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
       >
         <div className="absolute inset-0"></div>
-        {/* <h4 className="my-3 text-xl font-semibold tracking-wide text-white relative z-10">
-          Activités de l'atelier Arcadie
-        </h4> */}
-        <div className=" absolute bottom-0 p-8">
-          <div className="grid gap-6 my-3 md:grid-cols-2 relative z-10">
-            {services.map((service) => (
-              <div
-                className="border-2 col-span-2 p-2 bg-zinc-200 rounded-lg dark:bg-dark-200 md:col-span-1  hover:border-gray-600"
-                key={service.title}
-              >
-                <ServiceCard service={service} />
-              </div>
-            ))}
+        <div className="flex flex-col justify-center items-center md:flex-row md:space-x-3">
+          <div className="mb-6 md:w-1/2 md:mb-0">
+            <Image
+              src={bannner1}
+              alt="Photo"
+              width="500"
+              height="500"
+              priority
+              className="max-w-full object-cover"
+            />
           </div>
+          <div className="md:w-1/2">
+            <Image
+              src={bannner2}
+              alt="Photo"
+              width="500"
+              height="500"
+              priority
+              className="max-w-full object-cover"
+            />
+          </div>
+        </div>
+        <div className="grid gap-6 my-3 md:grid-cols-2 relative z-10">
+          {services.map((service) => (
+            <div
+              className="border-2 col-span-2 p-2 bg-zinc-200 rounded-lg dark:bg-dark-200 md:col-span-1 hover:border-gray-600"
+              key={service.title}
+            >
+              <ServiceCard service={service} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
